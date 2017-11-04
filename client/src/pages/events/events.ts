18a1e1fc9bfa0f3648a34fa234da27241156557f
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-04 01:27:08 -0500

    11 - using backing data - BP

--- client/src/pages/events/events.ts
+++ client/src/pages/events/events.ts
@@ -15,11 +15,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventsPage {
 
+  public events:any[] = [
+    {name: 'Hack4Good', date: 'Nov 4', month: 'November 2017', favorite: true},
+    {name: 'Family Style Dinner', date: 'Nov 14', month: 'November 2017', favorite: false},
+    {name: 'Office Closed', date: 'Dec 23-26', month: 'December 2017', favorite: false},
+    {name: 'Event A', date: 'Jan 17', month: 'January 2018', favorite: true},
+    {name: 'Event B', date: 'Jan 23', month: 'January 2018', favorite: false},
+    {name: 'Event C', date: 'Jan 28', month: 'January 2018', favorite: false},
+    {name: 'Event D', date: 'Feb 3', month: 'February 2018', favorite: true},
+    {name: 'Event E', date: 'Feb 11', month: 'February 2018', favorite: false},
+    {name: 'Event F', date: 'Mar 17', month: 'March 2018', favorite: true},
+    {name: 'Event G', date: 'Apr 2', month: 'April 2018', favorite: false},
+    {name: 'Event H', date: 'Apr 8-10', month: 'April 2018', favorite: false},
+    {name: 'Event I', date: 'Apr 23', month: 'April 2018', favorite: false},
+    {name: 'Event J', date: 'Apr 25', month: 'April 2018', favorite: true},
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventsPage');
+  }
+
+  getEvents():any{
+    let months = [];
+
+    for(let event of this.events){
+      let foundMonth = months.filter(m=>m.name == event.month);
+      if(foundMonth.length == 0){
+        months.push({name: event.month, events: [event]});
+      }
+      else{
+        foundMonth[0].events.push(event);
+      }
+    }
+
+    return months;
+  }
+
+  toggleFavorite(event: any){
+    event.favorite = !event.favorite;
   }
 
 }
