50da4daa090946895d1bba49b179d91e798616b4
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-04 14:21:22 -0500

    12 - Added assignment view - JR MM

--- client/src/pages/curriculum/curriculum.ts
+++ client/src/pages/curriculum/curriculum.ts
@@ -15,12 +15,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 
 export class CurriculumPage {
+  private assignments1: any[] = [{ name: "Activity 1", isCompleted: false }, { name: "Activity 2", isCompleted: true }];
+  private assignments2: any[] = [{ name: "Worksheet 1", isCompleted: true }, { name: "Activity 3", isCompleted: true }];
+  private assignments3: any[] = [{ name: "Worksheet 2", isCompleted: false }];
 
-  private assignments1:any[] = [{name: "Activity 1", isCompleted: false},{name: "Activity 2", isCompleted: true}];
-  private assignments2:any[] = [{name: "Worksheet 1", isCompleted: true}, {name: "Activity 3", isCompleted: true}];
-  private assignments3:any[] = [{name: "Worksheet 2", isCompleted: false}];
-
-  private modules : Module[] = [
+  private modules: Module[] = [
     new Module(1, "Emotional Development", this.assignments1),
     new Module(2, "Identity Development", this.assignments2),
     new Module(3, "Employment Development", this.assignments3)
@@ -33,26 +32,32 @@ export class CurriculumPage {
     console.log('ionViewDidLoad CurriculumPage');
   }
 
-  getModules(): any[] {
-    return this.modules;
+  getModules(): any[] { 
+    return this.modules;    
   }
 
-  itemSelected(item:any): void{
-
+  toggleSelection(item: Module): void {
+    item.selected = !item.selected;
   }
+
+  selectAssignment(item: Assignment): void {
+    this.currentAssignment = item;
+  }
+
+  private currentAssignment: Assignment = null
 }
 
-export class Assignment
-{
+export class Assignment {
   name: string
   isCompleted: boolean
+  url: string
 }
 
-export class Module
-{
+export class Module {
+  public selected: boolean = false;
   constructor(public id: number, public name: string, public assignments: Assignment[]) { }
 
-  numberComplete() : number {
+  numberComplete(): number {
     return this.assignments.filter(x => x.isCompleted).length;
   }
 }
