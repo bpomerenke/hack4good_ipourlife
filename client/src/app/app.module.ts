249c224212c029dbe392bbf512069786128ba913
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-04 15:48:59 -0500

    17 - created empty modules and registered in app module - MM

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,13 @@ import { EventsPage } from '../pages/events/events';
 import { ResourcesPage} from '../pages/resources/resources';
 import { TabsPage } from '../pages/tabs/tabs';
 import { WishlistPage } from '../pages/wishlist/wishlist';
+
 import { CurriculumPage } from '../pages/curriculum/curriculum';
 import { WishlistCoachPage } from '../pages/wishlist/wishlist-coach';
 import { WishlistYouthPage } from '../pages/wishlist/wishlist-youth';
+import { CurriculumYouthPage } from '../pages/curriculum/curriculum-youth';
+import { CurriculumCoachPage } from '../pages/curriculum/curriculum-coach';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -27,7 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     CurriculumPage,
     WishlistPage,
     WishlistCoachPage,
-    WishlistYouthPage
+    WishlistYouthPage,
+    CurriculumYouthPage,
+    CurriculumCoachPage
   ],
   imports: [
     BrowserModule,
@@ -43,7 +49,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage,
     WishlistPage,
     CurriculumPage,
-    WishlistYouthPage
+    WishlistYouthPage,
+    CurriculumYouthPage,
+    CurriculumCoachPage
   ],
   providers: [
     StatusBar,
