f7a08bf1f9a2f0ae92897c80f167b8d63ff28d48
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-04 15:36:38 -0500

    12 [CR] - deleted unused dependencies - JG MM

--- client/src/pages/tabs/tabs.ts
+++ client/src/pages/tabs/tabs.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { EventsPage } from '../events/events';
 import { ResourcesPage } from '../resources/resources';
