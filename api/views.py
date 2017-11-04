dd8e34f70a48df0c37deba1f1aebd88ce40dab28
Ben Pomerenke <ben.pomerenke@asynchrony.com>
2017-11-04 14:31:11 -0500

    18 - example of parsing json from request - BP

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from api.models import Person, generate_token_id, AccountToken
+from api.models import Person, generate_token_id, AccountToken, FooObject
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     newToken = AccountToken.objects.create(first_name="Bob", last_name="Dude", phone_number="417-355-1079")
     return HttpResponse("Hello, world. You're at the api index. " + generate_token_id() + " person exists")
+
+@csrf_exempt 
+def postCheck(request):
+    body_unicode = request.body.decode('utf-8')
+    data = json.loads(body_unicode)
+
+    return HttpResponse("body: %s" % data['name'])
