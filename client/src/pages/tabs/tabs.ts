f64599e2521705fb4b6efe63fb690743fd934a1c
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-04 00:20:39 -0500

    11 - basic tabs - BP

--- client/src/pages/tabs/tabs.ts
+++ client/src/pages/tabs/tabs.ts
@@ -9,9 +9,11 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  eventsRoot = AboutPage;
+  resourcesRoot = AboutPage;
+  contactsRoot = ContactPage;
+  curriculumRoot = AboutPage;
+  wishlistRoot = AboutPage;
 
   constructor() {
 
