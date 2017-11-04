4ab7ac983ad3fb3844367099f14dd0ed32ba534a
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-04 17:41:45 -0500

    17 - backfill user provider for wishlist - BP

--- client/src/pages/wishlist/wishlist.ts
+++ client/src/pages/wishlist/wishlist.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'page-wishlist',
   templateUrl: 'wishlist.html'
 })
 export class WishlistPage {
+  constructor(private userProvider: UserProvider){
 
+  }
+
+  getUserType(): string{
+    return this.userProvider.currentUser.type;
+  }
 }
