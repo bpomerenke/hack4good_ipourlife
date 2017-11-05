db061238e6f7d2c66d5abc8911bb824c0af2c2e7
Ben Pomerenke <pomerenb@asynchrony.com>
2017-11-05 00:39:57 -0500

    34 - updating model defs to have pretty name - BP

--- api/models.py
+++ api/models.py
@@ -28,12 +28,18 @@ class PersonType(ChoiceEnum):
     COACH = 2
 
 class Contact(models.Model):
+    def __str__(self):
+        return 'Contact: ' + self.name
+
     objects = models.Manager()
     number = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
     img = models.CharField(max_length=4000, default="")
 
 class Person(models.Model):
+    def __str__(self):
+        return 'Person: ' + self.user.username + '(' + self.person_type + ')'
+
     objects = models.Manager()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     person_type = models.CharField(max_length=1, choices=PersonType.choices(), default='1', null=False)
@@ -46,6 +52,9 @@ def generate_token_id():
     return str(b32)[2:6]
 
 class AccountToken(models.Model):
+    def __str__(self):
+        return 'Token: ' + self.token_id + ' - ' + self.first_name + ' ' + self.last_name
+
     objects = models.Manager()
     generated_at = models.DateTimeField(default=datetime.now, blank=True)
     used = models.BooleanField(default=False)
@@ -66,6 +75,9 @@ class AccountToken(models.Model):
         return newUser
 
 class Wish(models.Model):
+    def __str__(self):
+        return 'Wish: ' + self.name + ' (for ' + self.person.user.username + ')'
+    
     objects = models.Manager()
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
