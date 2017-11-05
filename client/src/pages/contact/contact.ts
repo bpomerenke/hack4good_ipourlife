4f7e7d6ecdc80815763b700ca810c34879055f03
Roberts <robertjo@wwt.com>
2017-11-04 20:49:27 -0500

    25 [CR] - removed unused import - MM

--- client/src/pages/contact/contact.ts
+++ client/src/pages/contact/contact.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-// import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-contact',
